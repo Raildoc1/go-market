a7fe2c3428dffa5fa89c89b6b0728433c0a0304b
Kirill Gaiduk <raildoc@Kirills-MacBook-Pro.local>
2025-02-15 04:02:17 +0300

    wip

--- internal/gophermart/accrualsystem/accrual-system.go
+++ internal/gophermart/accrualsystem/accrual-system.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"go-market/internal/common/accrualsystemprotocol"
+	"go-market/pkg/logging"
+	"go.uber.org/zap"
 )
 
 var (
@@ -18,10 +20,10 @@ type Config struct {
 }
 type AccrualSystem struct {
 	cfg    Config
-	logger resty.Logger
+	logger *logging.ZapLogger
 }
 
-func NewAccrualSystem(cfg Config, logger resty.Logger) *AccrualSystem {
+func NewAccrualSystem(cfg Config, logger *logging.ZapLogger) *AccrualSystem {
 	return &AccrualSystem{
 		cfg:    cfg,
 		logger: logger,
@@ -33,7 +35,6 @@ func (as *AccrualSystem) GetOrderStatus(ctx context.Context, orderNumber string)
 	resp, err := resty.
 		New().
 		R().
-		SetLogger(as.logger).
 		SetContext(ctx).
 		SetPathParam("number", orderNumber).
 		Get(url)
@@ -43,13 +44,17 @@ func (as *AccrualSystem) GetOrderStatus(ctx context.Context, orderNumber string)
 	statusCode := resp.StatusCode()
 	switch statusCode {
 	case 204:
+		as.logger.DebugCtx(ctx, "No order found")
 		return accrualsystemprotocol.Order{}, ErrNoOrderFound
 	case 200:
+		as.logger.DebugCtx(ctx, "Order found")
 		res := accrualsystemprotocol.Order{}
 		err := json.Unmarshal(resp.Body(), &res)
 		if err != nil {
+			as.logger.ErrorCtx(ctx, "Error unmarshalling order response", zap.Error(err))
 			return accrualsystemprotocol.Order{}, err
 		}
+		as.logger.DebugCtx(ctx, "Order found", zap.Any("order", res))
 		return res, nil
 	default:
 		return accrualsystemprotocol.Order{}, fmt.Errorf("unexpected status code %v", statusCode)
